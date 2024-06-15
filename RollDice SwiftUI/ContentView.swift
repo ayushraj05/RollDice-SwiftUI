b30b04ffc2e62a341d0ed6fd174065d6f590fa68
Ayush Rajpal <ayushrajpal2021@gmail.com>
2024-06-15 16:16:41 +0530

    complete the app

--- RollDice SwiftUI/ContentView.swift
+++ RollDice SwiftUI/ContentView.swift
@@ -8,17 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Image("diceeLogo")
+//                    .padding(100)
+                    Spacer()
+                HStack{
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                    
+                }, label: {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                        .background(Color.red)
+                        .cornerRadius(10)
+                })
+//                Button(action: {
+//                    
+//                }) {
+//                    Text("Roll")
+//                        .font(.system(size: 50))
+//                        .fontWeight(.heavy)
+//                        .foregroundColor(.white)
+//                        .padding(.horizontal)
+//                }
+//                .background(Color.red)
+            }
         }
-        .padding()
     }
 }
-
+    
 #Preview {
     ContentView()
+}
+
+struct DiceView: View {
+    let n: Int
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1,contentMode: .fit)
+//            .frame(width: 100, height: 100)
+            .padding()
+    }
 }
