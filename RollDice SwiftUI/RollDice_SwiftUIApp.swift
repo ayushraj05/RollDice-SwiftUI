1838472b713d3f55d98a14ab033161d0f6ec7f42
Ayush Rajpal <ayushrajpal2021@gmail.com>
2024-06-15 14:40:43 +0530

    Initial Commit

---
+++ RollDice SwiftUI/RollDice_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollDice_SwiftUIApp.swift
+//  RollDice SwiftUI
+//
+//  Created by Ayush Rajpal on 15/06/24.
+//
+
+import SwiftUI
+
+@main
+struct RollDice_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
